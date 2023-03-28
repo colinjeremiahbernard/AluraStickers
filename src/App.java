1ae8b3d431abfbd8baecb1fe22d65914e8739e03
COLIN STEPHENSON <colinjeremiahbernard@gmail.com>
2023-03-28 03:06:27 -0300

    Getting on the way with a java project imdb

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //fazer uma conexao http e buscar os top 250 filmes.
+        String url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        
+//extrair os dados que interessam (titulo, poster, classificação).
+        
+        var parser = new JsonParser();
+        List<Map<String, String>> ListaDeFilmes = parser.parse(body);
+        
+        //exibir e manipular os dados.
+        for (Map<String, String> filme : ListaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imdbRating"));
+    }
+    
+    }
+}
---
+++ src/responseBodyHandler.java
@@ -0,0 +1,9 @@
+import java.net.http.HttpResponse.BodyHandlers;
+
+public interface responseBodyHandler {
+
+  static BodyHandlers ofString() {
+    return null;
+  }
+
+}
