5e6640cd8ddb209ba726d8eb436d4b2c2c6b10b7
COLIN STEPHENSON <colinjeremiahbernard@gmail.com>
2023-03-30 01:39:44 -0300

    completed inputStream

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -23,9 +25,13 @@ public class App {
         
         //exibir e manipular os dados.
         for (Map<String, String> filme : ListaDeFilmes) {
+            String urlImagem = filme.get("imagem");
+            InputStream inputStream = new URL(urlImagem).openStream();
+            var generator = new StickerGenerator();
+            generator.cria(inputStream, urlImagem);
             System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
             System.out.println(filme.get("imdbRating"));
+            System.out.println();
     }
     }
 }
